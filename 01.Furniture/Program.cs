717b2178d18bb832b84f8cf3c658e16a4943d924
Ivaylo Moraliev <ivaylo.moraliev@gmail.com>
2022-08-23 22:50:11 +0300

    Add project files.

---
+++ 01.Furniture/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01.Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            //string pattern = @">{2}(?<name>\w+)<{2}(?<price>[0-9.]+|\[0-9]+)!(?<quantity>\d+)";
+            string pattern = @">{2}(?<name>\w+)<{2}(?<price>\d+\.\d+|\d+)!(?<quantity>\d+)";
+            double sum = 0;
+
+            List<string> orders = new List<string>();
+
+            while (input != "Purchase")
+            {
+                orders.Add(input);
+                input = Console.ReadLine();
+            }
+
+            MatchCollection validPurchases = Regex.Matches(string.Join(" ", orders), pattern);
+
+            Console.WriteLine($"Bought furniture:");
+
+            foreach (Match furniturePcs in validPurchases)
+            {
+                string furniture = furniturePcs.Groups["name"].Value;
+                int quantity = int.Parse(furniturePcs.Groups["quantity"].Value);
+                double price = double.Parse(furniturePcs.Groups["price"].Value);
+
+                sum += price * quantity;
+
+                Console.WriteLine(furniture);
+            }
+
+            Console.WriteLine($"Total money spend: {sum:f2}");
+
+        }
+    }
+}
---
+++ 02.Race/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> participants = new Dictionary<string, int>();
+
+            string pattern1 = @"[A-Z]|[a-z]";
+            string pattern2 = @"[0-9]";
+
+            List<string> participantNames = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string input = Console.ReadLine();
+
+            while (input != "end of race")
+            {
+                MatchCollection names = Regex.Matches(input, pattern1);
+                MatchCollection distances = Regex.Matches(input, pattern2);
+
+                string name = string.Join("", names);
+                int distance = 0;
+
+                foreach (Match item in distances)
+                {
+                    distance += int.Parse(item.Value);
+                }
+
+                if (!participants.ContainsKey(name) && participantNames.Contains(name))
+                {
+                    participants.Add(name, 0);
+                }
+
+                if (participants.ContainsKey(name))
+                {
+                    participants[name] += distance;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            participants = participants.OrderByDescending(r => r.Value).ToDictionary(k => k.Key, v => v.Value);
+
+            Console.WriteLine($"1st place: {participants.ElementAt(0).Key}");
+            Console.WriteLine($"2nd place: {participants.ElementAt(1).Key}");
+            Console.WriteLine($"3rd place: {participants.ElementAt(2).Key}");
+
+        }
+    }
+}
